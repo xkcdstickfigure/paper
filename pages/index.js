ec038057f246d7a282dc498c544271346becadd1
Archie Baer <archie@abaer.dev>
2020-02-18 00:31:52 +0000

    Homepage Greeting

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,28 @@
 import Page from "../components/Page";
+import theme from "../theme";
 
 export default () => {
 	return (
-		<Page
-			breadcrumbs={[
-				{
-					name: "@archie",
-					href: "/test"
-				},
-				{
-					name: "Hello World"
+		<Page>
+			<section className="greeting">
+				<h1>Hello, Archie.</h1>
+			</section>
+
+			<style jsx>{`
+				section.greeting {
+					background: white;
+					padding: 20px 50px;
+					border: solid 1px ${theme.borderGrey};
+					max-width: 1000px;
+					margin: 20px auto;
+					box-sizing: border-box;
+					overflow: auto;
 				}
-			]}
-		>
-			<p>test</p>
+
+				section.greeting h1 {
+					font-size: 30px;
+				}
+			`}</style>
 		</Page>
 	);
 };
