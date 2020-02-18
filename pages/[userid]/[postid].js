7d889ce8500d89260d02e7540cf5efce1c1e1cbd
Archie Baer <archie@abaer.dev>
2020-02-18 01:12:39 +0000

    Formatting

--- pages/[userid]/[postid].js
+++ pages/[userid]/[postid].js
@@ -2,21 +2,19 @@ import Page from "../../components/Page";
 import theme from "../../theme";
 
 const postPage = props => (
-    <Page
-        title="Building Paper"
-        breadcrumbs={[
-            {
-                name: "@archie",
-                href: "/[userid]",
-                as: "/archie"
-            },
-            {
-                name: "Building Paper"
-            }
-        ]}
-    >
-
-    </Page>
+	<Page
+		title="Building Paper"
+		breadcrumbs={[
+			{
+				name: "@archie",
+				href: "/[userid]",
+				as: "/archie"
+			},
+			{
+				name: "Building Paper"
+			}
+		]}
+	></Page>
 );
 
 export default postPage;
--- pages/[userid]/index.js
+++ pages/[userid]/index.js
@@ -2,16 +2,14 @@ import Page from "../../components/Page";
 import theme from "../../theme";
 
 const userPage = props => (
-    <Page
-        title="@archie"
-        breadcrumbs={[
-            {
-                name: "@archie"
-            }
-        ]}
-    >
-
-    </Page>
+	<Page
+		title="@archie"
+		breadcrumbs={[
+			{
+				name: "@archie"
+			}
+		]}
+	></Page>
 );
 
 export default userPage;
