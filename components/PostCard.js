3551d2664bc4bada44689428a45b231bc07e35da
Archie Baer <archie@abaer.dev>
2020-02-18 00:53:07 +0000

    Post Card component

---
+++ components/PostCard.js
@@ -0,0 +1,48 @@
+import theme from "../theme";
+
+import Link from "next/link";
+
+export default props => (
+    <Link href="/p/[user]/[id]" as={`/p/${props.username}/${props.slug}`}>
+        <a>
+            <div className="post">
+                <img src={props.image} />
+                <h1>{props.title}</h1>
+                <h2>@{props.username}</h2>
+                <p>{props.excerpt}</p>
+
+                <style jsx>{`
+                    .post {
+                        border: solid 1px ${theme.borderGrey};
+                        background: white;
+                        width: 300px;
+                        box-sizing: border-box;
+                        padding: 15px;
+                        margin: 30px auto;
+                    }
+
+                    img {
+                        width: 100%;
+                        border-radius: 10px;
+                    }
+
+                    h1 {
+                        font-size: 20px;
+                        margin: 10px 0 0 0;
+                    }
+
+                    h2 {
+                        margin: 0 0 10px 0;
+                        font-size: 15px;
+                        font-weight: 300;
+                    }
+
+                    p {
+                        margin: 0;
+                        color: ${theme.grey4};
+                    }
+                `}</style>
+            </div>
+        </a>
+    </Link>
+);
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,22 @@
 import Page from "../components/Page";
 import theme from "../theme";
 
+import Post from "../components/PostCard";
+
 export default () => {
 	return (
 		<Page>
 			<section className="greeting">
 				<h1>Hello, Archie.</h1>
 			</section>
+
+			<Post
+				title="Building Paper"
+				username="archie"
+				slug="building-paper"
+				image="https://thumbor.forbes.com/thumbor/960x0/https%3A%2F%2Fblogs-images.forbes.com%2Fforbestechcouncil%2Ffiles%2F2019%2F01%2Fcanva-photo-editor-8-7.jpg"
+				excerpt="Twitter. It's a place for short, quick-fire thoughts. It's nice, but full of ads, people tend to be toxic, and it's far from an alternative to blogging..."
+			/>
 
 			<style jsx>{`
 				section.greeting {
