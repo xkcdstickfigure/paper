e6d30cd4eb4f68e56976efc9304eed236f7315dd
Archie Baer <archie@abaer.dev>
2020-03-19 22:45:38 +0000

    config.inputBounds

--- config.js
+++ config.js
@@ -2,5 +2,19 @@ const dev = process.env.NODE_ENV === "development";
 
 export default {
 	apiUrl: `${dev ? "http://localhost" : "https://paper.alles.cx"}/api/v1`,
-	dev
+	dev,
+	inputBounds: {
+		title: {
+			min: 5,
+			max: 125
+		},
+		image: {
+			min: 16,
+			max: 255
+		},
+		content: {
+			min: 50,
+			max: 15000
+		}
+	}
 };
