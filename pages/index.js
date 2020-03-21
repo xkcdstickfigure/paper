f05fe22c21a02484be77eb58d21c365b53adf3cf
Archie Baer <archie@abaer.dev>
2020-03-21 13:50:09 +0000

    Sources homepage data from api

--- pages/index.js
+++ pages/index.js
@@ -2,120 +2,90 @@ import Page from "../components/Page";
 import withAuth from "../util/withAuth";
 import Post from "../components/PostCard";
 import NewButton from "../components/NewButton";
+import config from "../config";
+import axios from "axios";
 
 const Homepage = props => {
-	return (
-		<Page user={props.user}>
-			<section className="greeting">
-				<h1>
-					Hello, {props.user.nickname}
-					{props.user.plus ? <sup>+</sup> : <></>}
-				</h1>
-			</section>
-
-			<section className="posts">
-				{props.posts.map(post => (
-					<Post
-						key={post.id}
-						title={post.title}
-						username={post.username}
-						slug={post.slug}
-						image={post.image}
-						excerpt={post.excerpt}
-						style={{
-							width: "100%"
-						}}
-					/>
-				))}
-			</section>
-
-			<NewButton />
-
-			<style jsx>{`
-				section.greeting {
-					padding: 20px 19px;
-				}
-
-				section.greeting h1 {
-					font-size: 30px;
-				}
-
-				section.posts {
-					display: grid;
-					grid-template-columns: minmax(0, 1fr) minmax(0, 1fr) minmax(0, 1fr);
-				}
-
-				@media screen and (max-width: 750px) {
-					section.posts {
-						grid-template-columns: minmax(0, 1fr) minmax(0, 1fr);
+	if (props.posts) {
+		return (
+			<Page user={props.user}>
+				<section className="greeting">
+					<h1>
+						Hello, {props.user.nickname}
+						{props.user.plus ? <sup>+</sup> : <></>}
+					</h1>
+				</section>
+	
+				<section className="posts">
+					{props.posts.map(post => (
+						<Post
+							key={post.id}
+							title={post.title}
+							username={post.username}
+							slug={post.slug}
+							image={post.image}
+							excerpt={post.excerpt}
+							style={{
+								width: "100%"
+							}}
+						/>
+					))}
+				</section>
+	
+				<NewButton />
+	
+				<style jsx>{`
+					section.greeting {
+						padding: 20px 19px;
 					}
-				}
-
-				@media screen and (max-width: 500px) {
-					section.posts {
-						grid-template-columns: minmax(0, 1fr);
+	
+					section.greeting h1 {
+						font-size: 30px;
 					}
-				}
-			`}</style>
-		</Page>
-	);
+	
+					section.posts {
+						display: grid;
+						grid-template-columns: minmax(0, 1fr) minmax(0, 1fr) minmax(0, 1fr);
+					}
+	
+					@media screen and (max-width: 750px) {
+						section.posts {
+							grid-template-columns: minmax(0, 1fr) minmax(0, 1fr);
+						}
+					}
+	
+					@media screen and (max-width: 500px) {
+						section.posts {
+							grid-template-columns: minmax(0, 1fr);
+						}
+					}
+				`}</style>
+			</Page>
+		);
+	} else {
+		return (
+			<Page user={props.user}>
+				<p style={{textAlign: "center"}}>An error occurred</p>
+			</Page>
+		);
+	}
 };
 
-Homepage.getInitialProps = ctx => {
-	return {
-		posts: [
-			{
-				id: 0,
-				title: "Building Paper",
-				username: "archie",
-				slug: "building-paper",
-				image:
-					"https://thumbor.forbes.com/thumbor/960x0/https%3A%2F%2Fblogs-images.forbes.com%2Fforbestechcouncil%2Ffiles%2F2019%2F01%2Fcanva-photo-editor-8-7.jpg",
-				excerpt:
-					"Twitter. It's a place for short, quick-fire thoughts. It's nice, but full of ads, people tend to be toxic, and it's far from an alternative to blogging..."
-			},
-			{
-				id: 1,
-				title: "Building Paper",
-				username: "archie",
-				slug: "building-paper",
-				image:
-					"https://thumbor.forbes.com/thumbor/960x0/https%3A%2F%2Fblogs-images.forbes.com%2Fforbestechcouncil%2Ffiles%2F2019%2F01%2Fcanva-photo-editor-8-7.jpg",
-				excerpt:
-					"Twitter. It's a place for short, quick-fire thoughts. It's nice, but full of ads, people tend to be toxic, and it's far from an alternative to blogging..."
-			},
-			{
-				id: 2,
-				title: "Building Paper",
-				username: "archie",
-				slug: "building-paper",
-				image:
-					"https://thumbor.forbes.com/thumbor/960x0/https%3A%2F%2Fblogs-images.forbes.com%2Fforbestechcouncil%2Ffiles%2F2019%2F01%2Fcanva-photo-editor-8-7.jpg",
-				excerpt:
-					"Twitter. It's a place for short, quick-fire thoughts. It's nice, but full of ads, people tend to be toxic, and it's far from an alternative to blogging..."
-			},
-			{
-				id: 3,
-				title: "Building Paper",
-				username: "archie",
-				slug: "building-paper",
-				image:
-					"https://thumbor.forbes.com/thumbor/960x0/https%3A%2F%2Fblogs-images.forbes.com%2Fforbestechcouncil%2Ffiles%2F2019%2F01%2Fcanva-photo-editor-8-7.jpg",
-				excerpt:
-					"Twitter. It's a place for short, quick-fire thoughts. It's nice, but full of ads, people tend to be toxic, and it's far from an alternative to blogging..."
-			},
-			{
-				id: 4,
-				title: "Building Paper",
-				username: "archie",
-				slug: "building-paper",
-				image:
-					"https://thumbor.forbes.com/thumbor/960x0/https%3A%2F%2Fblogs-images.forbes.com%2Fforbestechcouncil%2Ffiles%2F2019%2F01%2Fcanva-photo-editor-8-7.jpg",
-				excerpt:
-					"Twitter. It's a place for short, quick-fire thoughts. It's nice, but full of ads, people tend to be toxic, and it's far from an alternative to blogging..."
+Homepage.getInitialProps = async ctx => {
+	var apiReq;
+	try {
+		apiReq = await axios.get(`${config.apiUrl}/posts`, {
+			headers: {
+				authorization: ctx.user.sessionToken
 			}
-		]
-	};
+		});
+	} catch (err) {
+		return;
+	}
+
+	return {
+		posts: apiReq.data.posts
+	}
 };
 
 export default withAuth(Homepage);
