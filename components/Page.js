703dbccdbb4571c1154611d3a468fb44966dfae6
Archie Baer <archie@abaer.dev>
2020-04-17 01:39:03 +0100

    Console Warning

--- components/Page.js
+++ components/Page.js
@@ -2,44 +2,50 @@ import theme from "../reactants/theme";
 import Head from "../reactants/Head";
 import Header from "../reactants/Header";
 import GlobalStyles from "../reactants/GlobalStyles";
+import consoleWarning from "../reactants/consoleWarning";
+import {useEffect} from "react";
 
-export default props => (
-	<div className="page">
-		<Head title={props.title ? `Paper // ${props.title}` : `Paper`}>
-			<link rel="shortcut icon" href="/icon.png" />
-		</Head>
+export default props => {
+	useEffect(consoleWarning, []);
 
-		<Header
-			title="Paper"
-			userId={props.user ? props.user.id : null}
-			breadcrumbs={props.breadcrumbs}
-		/>
-		<main>
-			<div className="innerMain">{props.children}</div>
-		</main>
-
-		<style jsx>{`
-			.page {
-				display: flex;
-				flex-flow: column;
-			}
-
-			main {
-				padding: 20px;
-				flex-grow: 1;
-				background: ${theme.greyF};
-			}
-
-			.innerMain {
-				max-width: 1000px;
-				margin: 20px auto;
-			}
-		`}</style>
-
-		<style jsx global>{`
-			@import url("/nprogress.css");
-		`}</style>
-
-		<GlobalStyles />
-	</div>
-);
+	return (
+		<div className="page">
+			<Head title={props.title ? `Paper // ${props.title}` : `Paper`}>
+				<link rel="shortcut icon" href="/icon.png" />
+			</Head>
+	
+			<Header
+				title="Paper"
+				userId={props.user ? props.user.id : null}
+				breadcrumbs={props.breadcrumbs}
+			/>
+			<main>
+				<div className="innerMain">{props.children}</div>
+			</main>
+	
+			<style jsx>{`
+				.page {
+					display: flex;
+					flex-flow: column;
+				}
+	
+				main {
+					padding: 20px;
+					flex-grow: 1;
+					background: ${theme.greyF};
+				}
+	
+				.innerMain {
+					max-width: 1000px;
+					margin: 20px auto;
+				}
+			`}</style>
+	
+			<style jsx global>{`
+				@import url("/nprogress.css");
+			`}</style>
+	
+			<GlobalStyles />
+		</div>
+	);
+};
